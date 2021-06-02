403b4b6e4e7d520507f746d8ee11835db9d97481
Daniel Ong <dyo20@ic.ac.uk>
2021-06-02 20:18:27 +0300

    fixed CPSR edits in data processing and fixed print_state

--- src/emulate.c
+++ src/emulate.c
@@ -108,7 +108,7 @@ uint32_t shift(uint32_t offset, bool set_condition) {
 	}
 
 	if (set_condition && shift_value != 0) {
-		if (shift_type == 0) 
+		if (shift_type == 0)
 			set_bit(Registers + CPSR_REGISTER, CPSR_Z, (register_value >> (31 - shift_value)) & 1);
 		else
 			set_bit(Registers + CPSR_REGISTER, CPSR_Z, (register_value >> shift_value) & 1);
@@ -138,9 +138,11 @@ void dataProcessing(uint32_t instruction) {
 	uint8_t opcode = (instruction >> 21) & 0b1111;
 	bool immediate_operand = (instruction >> 25) & 1;
 
-	uint32_t result = 0;
+	uint64_t result = 0;
 
-	bool isLogic = false;
+	bool is_arithmetic = false;
+	bool is_addition = false;
+	bool write_result = false;
 
 	if (immediate_operand) {
 		// operand2 is an immediate constant
@@ -148,31 +150,34 @@ void dataProcessing(uint32_t instruction) {
 		uint8_t imm = operand2 & 0xff;
 		operand2 = rotate_right(imm, rotate << 1); // Shifted 1 to the left to multiply by 2
 	}
-	else
+	else {
 		operand2 = shift(operand2, set_condition); //operand2 is a shifted register
+	}
 
 	switch (opcode) {
 	case 0b0000: //and
 		result = Registers[register_n] & operand2;
-		Registers[register_d] = result;
-		isLogic = true;
+		write_result = true;
 		break;
 	case 0b0001: //eor
 		result = Registers[register_n] ^ operand2;
-		Registers[register_d] = result;
-		isLogic = true;
+		write_result = true;
 		break;
 	case 0b0010: //sub
 		result = Registers[register_n] - operand2;
-		Registers[register_d] = result;
+		is_arithmetic = true;
+		write_result = true;
 		break;
 	case 0b0011: //rsb
 		result = operand2 - Registers[register_n];
-		Registers[register_d] = result;
+		is_arithmetic = true;
+		write_result = true;
 		break;
 	case 0b0100: //add
-		result = operand2 + Registers[register_n];
-		Registers[register_d] = result;
+		result = (uint64_t)operand2 + Registers[register_n];
+		is_addition = true;
+		is_arithmetic = true;
+		write_result = true;
 		break;
 	case 0b1000: //tst
 		result = Registers[register_n] & operand2;
@@ -182,38 +187,39 @@ void dataProcessing(uint32_t instruction) {
 		break;
 	case 0b1010: //cmp
 		result = Registers[register_n] - operand2;
+		is_arithmetic = true;
 		break;
 	case 0b1100: //orr
 		result = Registers[register_n] | operand2;
-		Registers[register_d] = result;
-		isLogic = true;
+		write_result = true;
 		break;
 	case 0b1101: //mov
 		result = operand2;
-		Registers[register_d] = result;
+		write_result = true;
 		break;
 	}
-	
+
+	if (write_result) {
+		Registers[register_d] = (uint32_t)result;
+	}
+
 	if (set_condition) {
 		// For C bit: if logic operation -> set to carry out from any shift operation (result from barrel shifter)
 		//            if arithmetic      -> set the the carry out from the ALU (1 for addition if there was a carry,
 		//																		0 for subtraction if there was a borrow)
-		if(!isLogic) {
-			if ((opcode == 0b0100) && (Registers[16] & 0b1)) {
-				set_bit(Registers + CPSR_REGISTER, CPSR_C, 0b1);
-			}
-			else if ((opcode == 0b0010) || (opcode == 0b0011) && (Registers[16] & 0b1)) {
-				set_bit(Registers + CPSR_REGISTER, CPSR_C, 0b1);
+		if (is_arithmetic) {
+			if (is_addition) {
+				set_bit(Registers + CPSR_REGISTER, CPSR_C, result > 0xffffffff);
 			}
 			else {
-				set_bit(Registers + CPSR_REGISTER, CPSR_C, 0b0);
+				set_bit(Registers + CPSR_REGISTER, CPSR_C, result <= 0xffffffff);
 			}
 		}
 
 		// Z = 1 if the result is all 0s 
-		if(result == 0) 
+		if (result == 0) {
 			set_bit(Registers + CPSR_REGISTER, CPSR_Z, 1);
-
+		}
 		// N = logical value of 31st bit of result
 		set_bit(Registers + CPSR_REGISTER, CPSR_N, (result >> 31) & 1);
 	}
@@ -250,29 +256,38 @@ void singleDataTransfer(uint32_t instruction) {
 	uint32_t offset = instruction & 0xfff;
 	uint32_t memory = Registers[register_n];
 
-	if (immediate_offset) 
+	if (immediate_offset)
 		offset = shift(offset, false);
 
-	if (up) 
+	if (up)
 		offset = Registers[register_n] + offset;
-	else 
+	else
 		offset = Registers[register_n] - offset;
 
-	if (pre_indexing) 
+	if (pre_indexing)
 		memory = offset;
 
-	if (load) 
+	if (load)
 		Registers[register_m] = read_ram(memory);
-	else 
+	else
 		write_ram(memory, Registers[register_m]);
 
-	if (!pre_indexing) 
+	if (!pre_indexing)
 		Registers[register_n] = offset;
 }
 
 void branch(uint32_t instruction) {
-	uint32_t offset = (instruction & 0xffffffU) << 2;
-	Registers[PC_REGISTER] += *((int32_t*)&offset);
+
+	uint32_t offset = (instruction & 0xffffff) << 2;
+	bool negative = (offset >> 25) & 1;
+	if (negative) {
+		offset = (~offset) + 1;
+		offset &= 0xffffff;
+		Registers[PC_REGISTER] -= offset;
+	}
+	else {
+		Registers[PC_REGISTER] += offset;
+	}
 }
 
 uint32_t fetch() {
@@ -285,11 +300,11 @@ uint32_t fetch() {
 
 // swap used to convert from big endian to little endian
 
-uint32_t swap (uint32_t num) {
-	uint32_t swapped = ((num>>24)&0xff) | // move byte 3 to byte 0
-            ((num<<8)&0xff0000) | // move byte 1 to byte 2
-            ((num>>8)&0xff00) | // move byte 2 to byte 1
-            ((num<<24)&0xff000000); // byte 0 to byte 3
+uint32_t swap(uint32_t num) {
+	uint32_t swapped = ((num >> 24) & 0xff) | // move byte 3 to byte 0
+		((num << 8) & 0xff0000) | // move byte 1 to byte 2
+		((num >> 8) & 0xff00) | // move byte 2 to byte 1
+		((num << 24) & 0xff000000); // byte 0 to byte 3
 	return swapped;
 }
 
@@ -297,15 +312,15 @@ void print_state(uint16_t program_size) {
 	printf("Registers:\n");
 	uint16_t i;
 	for (i = 0; i <= 9; i++)
-		printf("$%d  :%11d (0x%08x)\n", i, Registers[i], Registers[i]);
+		printf("$%d  : %10d (0x%08x)\n", i, Registers[i], Registers[i]);
 	for (i = 10; i <= 12; i++)
-		printf("$%d :%11d (0x%08x)\n", i, Registers[i], Registers[i]);
+		printf("$%d : %10d (0x%08x)\n", i, Registers[i], Registers[i]);
 
-	printf("PC  :%11d (0x%08x)\n", Registers[PC_REGISTER], Registers[PC_REGISTER]);
-	printf("CPSR:%11d (0x%08x)\n", Registers[CPSR_REGISTER], Registers[CPSR_REGISTER]);
+	printf("PC  : %10d (0x%08x)\n", Registers[PC_REGISTER], Registers[PC_REGISTER]);
+	printf("CPSR: %10d (0x%08x)\n", Registers[CPSR_REGISTER], Registers[CPSR_REGISTER]);
 	printf("Non-zero memory:\n");
 	for (i = 0; i <= 65532; i += 4) {
-		if(read_ram(i) != 0)
+		if (read_ram(i) != 0)
 			printf("0x%08x: 0x%08x\n", i, swap(read_ram(i))); // if you want to view numbers in regular big endian format, delete swap here
 		if (i == 65532) {
 			break;
