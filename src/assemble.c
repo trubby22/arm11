e2c83bfb308372f695b6548f2c43217ee7943c65
michael mabo <mm920@gpu10.doc.ic.ac.uk>
2021-06-11 20:00:04 +0100

    branch bug fix

--- src/assemble.c
+++ src/assemble.c
@@ -302,16 +302,16 @@ uint32_t singleDataTransfer(char* label, int* opcode, uint32_t** operands, uint3
 // the target address passed in to this function is either a specific 32-bit 
 // address or a label.
 uint32_t branch(char* mnemonic, const int current_address, const int target_address) {
-	uint32_t  result, opcode = string_to_opcode(mnemonic) % 10;
+	uint32_t  result, cond = string_to_opcode(mnemonic) % 10;
 	int32_t offset;
 	const uint32_t unchanged_bits = 0xa << 24;
 
-	if (strcmp("b", mnemonic)) {
-		opcode--;
+	if (strcmp("b", mnemonic) == 0) {
+		cond--;
 	}
-	if (opcode > 1) {
-		// opcode = bge, blt, bgt, ble, OR bal
-		opcode |= 0x8;
+	if (cond > 1) {
+		// cond = bge, blt, bgt, ble, OR bal
+		cond |= 0x8;
 	}
 
 	offset = target_address - current_address - 8;
@@ -319,7 +319,7 @@ uint32_t branch(char* mnemonic, const int current_address, const int target_addr
 	offset >>= 2;
 	offset &= 0xffffff; // store the lower 24 bits
 	
-	result = (0xe << 28) | unchanged_bits | offset;
+	result = (cond << 28) | unchanged_bits | offset;
 	//result = result & ~(0x10000000); // changes cond from 1111 to 1110
 	return result;
 }
